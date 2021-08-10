04794aad5db6e3532c915c8c4022241bffb8efe2
Shawna Akiona <shawna.akiona@gmail.com>
2021-08-10 10:03:51 -0700

    commit tree 6cbc58e0603afa97f5bfcaffc45e9f69ff702e3d

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someValue){
+  if (someValue <= 400) {
+    return 'This one is on me!';
+  } else if (someValue > 2000 && someValue < 2499) {
+    return 'I will gladly take your thirty bucks.';
+  } else {
+    return 'No can do.';
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+if (city === NYC) {
+  return 'Ok, sounds good.'
+} else {
+  return 'No go.';
+ }
 }
 
 function switchOnCharmFromTip(){
